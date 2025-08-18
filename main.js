cb9ee7eeffe1f15e31dc99a8fc5ffa485d7283a7
Daniel Steman <daniel-steman@live.nl>
2025-08-18 16:06:58 +0200

    fix chrome bin

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const { Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
-const client = new Client({puppeteer: {executablePath: "chrome-bin/chrome-mac/Chromium.app/Contents/MacOS/Chromium"}});
+const client = new Client({puppeteer: {executablePath: "chrome-mac/Chromium.app/Contents/MacOS/Chromium"}});
 
 client.on('ready', () => {
     console.log('Client is ready!');
