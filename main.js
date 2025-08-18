ea73fdebc5703323cf8aa4b61e7d2e454244732a
Daniel Steman <daniel-steman@live.nl>
2025-08-18 16:25:40 +0200

    get contact

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
-const { Client } = require('whatsapp-web.js');
-const qrcode = require('qrcode-terminal');
+const { Client } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
 
-const client = new Client({puppeteer: {executablePath: "chrome-mac/Chromium.app/Contents/MacOS/Chromium"}});
-
-client.on('ready', () => {
-    console.log('Client is ready!');
+const client = new Client({
+  puppeteer: {
+    executablePath: "chrome-mac/Chromium.app/Contents/MacOS/Chromium",
+  },
 });
 
-client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
+client.on("ready", () => {
+  console.log("Client is ready!");
 });
-client.on('message_create', message => {
-	console.log(message.body);
+
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("message_create", async (message) => {
+  const contact = await client.getContactById(message._data.from);
+  console.log(message._data);
+  console.log(contact);
+  console.log(message.body);
 });
 
 client.initialize();
-
